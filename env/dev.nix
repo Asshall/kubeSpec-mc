030f51f493ee08dd39ef863f2398b0afe4e5f4c9
Asshall <predateo@gmail.com>
2026-04-21 22:42:26 +0200

    Added gitignore ; Started traefik helm file

--- env/dev.nix
+++ env/dev.nix
@@ -1,6 +1,47 @@
+{ lib, ... }:
 {
   # Where should the generated manifests be stored?
   nixidy.target.repository = "https://github.com/Asshall/kubernix-mc";
   nixidy.target.branch = "main";
   nixidy.target.rootPath = "./manifests/dev";
+  applications.traefik = {
+    namespace = "traefik";
+
+    helm.releases.traefik = {
+
+      chart = lib.helm.downloadHelmChart {
+
+        chart = "traefik";
+        repo = "https://traefik.github.io/charts";
+        version = "v39.0.6";
+        chartHash = "sha256-drSIM1FsWRTHm2rLO8ceexg8HisKtyzwrIj+LZ+Gbo8=";
+      };
+      values = {
+        ingressRoute = {
+          dashboard = {
+            enabled = true;
+            matchRule = "Host(`dashboard.localhost`)";
+            entryPoints = [ "web" ];
+          };
+          providers = {
+            kubernetesGateway = {
+              enabled = true;
+            };
+          };
+          gateway = {
+            listeners = {
+              web = {
+                namespacePolicy = {
+                  from = "All";
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+
+    # Patch Helm output with nixidy
+    #resources.deployments.traefik.spec.replicas = lib.mkForce 5;
+  };
 }
